8ac620de1db97faf3233f61e9317bd7144f72e48
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-11-28 00:09:38 -0800

    Function is worked but output is not as expected

--- Alien Adventure/ShuffleStrings.swift
+++ Alien Adventure/ShuffleStrings.swift
@@ -9,6 +9,48 @@
 extension Hero {
     
     func shuffleStrings(s1: String, s2: String, shuffle: String) -> Bool {
-        return true
+        
+        var s1Array: [Character] = []
+        var s2Array: [Character] = []
+        var shuffleArray: [Character] = []
+        
+        func arrayOfString(string: String) -> ([Character]) {
+            var arrayOfCharacter: [Character] = []
+            for Character in string.characters {
+               arrayOfCharacter.append(Character)
+            }
+            return arrayOfCharacter
+        }
+        
+        s1Array = arrayOfString(string: s1)
+        s2Array = arrayOfString(string: s2)
+        
+        shuffleArray = arrayOfString(string: shuffle)
+        
+        var s1ArrayShuffled: [Character] = []
+        var s2ArrayShuffled: [Character] = []
+        
+        //validShuffle Function
+        func validShuffle(array: [Character]) -> [Character] {
+            
+            var shuffledArray: [Character] = []
+            
+            for arrayShuffledCharacter in shuffleArray {
+                for arrayCharacter in array {
+                    if arrayCharacter == arrayShuffledCharacter {
+                        shuffledArray.append(arrayShuffledCharacter)
+                    }
+                }
+            }
+            
+            return shuffledArray
+        }
+        
+        if s1Array == validShuffle(array: s1Array) && s2Array == validShuffle(array: s2Array) {
+            return true
+        } else {
+            return false
+        }
+        
     }
 }
