ff7b2f002aa232135ce9930e34ca00716a2005f1
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-11-27 21:26:29 -0800

    Inscription updated

--- Alien Adventure/InscriptionEternalStar.swift
+++ Alien Adventure/InscriptionEternalStar.swift
@@ -9,8 +9,15 @@
 extension Hero {
     
     func inscriptionEternalStar(inventory: [UDItem]) -> UDItem? {
-        return nil
+        
+        inventory.
+        for item in inventory {
+            if item.inscription == "THE ETERNAL STAR" {
+                
+            }
+        }
     }
 }
+
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 3"
